ff7f757248f0a3a11bf58b4d3a39117626e52891
munir sebikaaka <munirsebikaaka@gmail.com>
2024-08-27 12:45:58 +0300

    feat: added the inputs which will recieve the details of a user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,55 @@
+import { useState } from "react";
 export default function App() {
-  return <h1>CODESMAN NEW APP</h1>;
+  return (
+    <div>
+      <RegstrationCell />
+    </div>
+  );
+}
+
+function RegstrationCell() {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [district, setDistrict] = useState("");
+  // const [country, setCountry] = useState("codesmann");
+
+  const detailsArr = [
+    [{ type: "text", placeholder: "first name", value: { first } }],
+    [{ type: "text", placeholder: "last name", value: { last } }],
+    [{ type: "text", placeholder: "district", value: { district } }],
+    ["Uganda", "United States", "United Kingdom"],
+  ];
+  return (
+    <div>
+      {detailsArr[0].map((obj) => (
+        <input
+          type={obj.type}
+          placeholder={obj.placeholder}
+          value={first}
+          onChange={(e) => setFirst(e.target.value)}
+        />
+      ))}
+      {detailsArr[1].map((obj) => (
+        <input
+          type={obj.type}
+          placeholder={obj.placeholder}
+          value={last}
+          onChange={(e) => setLast(e.target.value)}
+        />
+      ))}
+      {detailsArr[2].map((obj) => (
+        <input
+          type={obj.type}
+          placeholder={obj.placeholder}
+          value={district}
+          onChange={(e) => setDistrict(e.target.value)}
+        />
+      ))}
+      <select>
+        {detailsArr[3].map((co) => (
+          <option>{co}</option>
+        ))}
+      </select>
+    </div>
+  );
 }
